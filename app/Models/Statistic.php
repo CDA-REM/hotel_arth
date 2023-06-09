c89deb8388d1a3b6a2696af8630d7e1de537c931
Emilie Ickart <emi.ickart@gmail.com>
2023-06-09 14:58:23 +0200

    Add card and statistic models

---
+++ app/Models/Statistic.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @property int id
+ * @property int card_id
+ * @property string traceability
+ */
+class Statistic extends Model
+{
+    use HasFactory;
+
+    public $timestamps = true;
+
+    public $fillable = ['card_id', 'traceability'];
+
+    /**
+     * Defines a relationship to card from the statistic table
+     * @return BelongsTo
+     */
+    public function card() : BelongsTo
+    {
+        return $this->belongsTo(Card::class);
+    }
+}
