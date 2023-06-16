08c549edb5d8a28e262962221832c70f6a198ce6
Emilie Ickart <emi.ickart@gmail.com>
2023-06-16 10:20:39 +0200

    Remove useless methods from StatisticController.php

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -16,42 +16,28 @@ use Illuminate\Support\Facades\Response;
 class StatisticController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the statistics of all the keycards sorted by key_card_id.
      *
-     * @return AnonymousResourceCollection
-     */
-    public function index(): AnonymousResourceCollection
-    {
-        return StatisticResource::collection(
-            Statistic::with('key_card_id')->paginate(10)
-        );
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @param Request $request
      * @return JsonResponse
      */
-    public function create(Request $request): JsonResponse
+    public function index(): JsonResponse
     {
         try {
-            $statistic = new Statistic;
-            $statistic->key_card_id = $request->key_card_id;
-            $statistic->traceability = json_encode([]);
-            $statistic->save();
-
+            $statistics = Statistic::orderBy('key_card_id')->paginate(10);
         } catch (Exception $e) {
             Log::error("A database error occured : {$e->getMessage()}");
-            return Response::json($e->getMessage(), 502);
+            return Response::json($e->getMessage(), 404);
         }
-
-        $resource = StatisticResource::make(Statistic::findorFail($statistic->id));
-        return response()->json($resource, 201);
+        // Return the collection of resources and a custom 200 message
+        return response()->json([
+            'data' => StatisticResource::collection($statistics),
+            'message' => 'Liste des statistiques des keycards',
+            'status' => 200
+        ]);
     }
 
     /**
-     * Display the specified resource.
+     * Display the statistics for the specified keycard.
      *
      * @param int $key_card_id
      * @return StatisticResource
@@ -59,35 +45,5 @@ class StatisticController extends Controller
     public function show(int $key_card_id): StatisticResource
     {
         return StatisticResource::make(Statistic::where('key_card_id', $key_card_id)->firstOrFail());
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \App\Http\Requests\UpdateStatisticRequest $request
-     * @param int $key_card_id
-     * @return StatisticResource
-     */
-    public function update(int $key_card_id, UpdateStatisticRequest $request): StatisticResource
-    {
-        $statistic = Statistic::where('key_card_id', $key_card_id)->firstOrFail();
-        $traceability = json_decode($statistic->traceability);
-        array_push($traceability, $request->opening_time);
-        log::info($traceability);
-        dd(json_decode($traceability));
-
-
-        return StatisticResource::make(Statistic::findOrFail($statistic->id));
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param Statistic $statistic
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Statistic $statistic)
-    {
-        //
     }
 }
