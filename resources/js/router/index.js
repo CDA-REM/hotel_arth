d2ccad071b9063e2fd7919a8da98f56ee22d2617
Emilie Ickart <emi.ickart@gmail.com>
2023-07-15 10:38:52 +0200

    Change router history mode to remove hash in url

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHashHistory} from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import LandingPage from '../Views/LandingPage/LandingPage';
 import Login from '../Views/Login';
 import SignUp from '../Views/SignUp';
@@ -60,7 +60,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(process.env.BASE_URL),
+    history: createWebHistory(process.env.BASE_URL),
     routes,
     scrollBehavior(to, from, savedPosition) {
         // if (savedPosition) {
