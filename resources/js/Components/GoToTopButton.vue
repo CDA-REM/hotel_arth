18e61d5711b844c57c63fdde8219d188d3fc8959
Emilie Ickart <emi.ickart@gmail.com>
2023-07-16 09:11:59 +0200

    Bf - Change GoToTopButton image

--- resources/js/Components/GoToTopButton.vue
+++ resources/js/Components/GoToTopButton.vue
@@ -1,7 +1,11 @@
 <template>
     <router-link to="#header">
-        <button type="button" :class="{ 'scrolled-page': scrolledPage}">
-            <img :src="image.source" :alt="image.alt">
+        <button type="button" :class="{ 'scrolled__page': scrolledPage}">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75" />
+            </svg>
+
+
         </button>
     </router-link>
 
@@ -43,22 +47,22 @@ export default {
         bottom: 0.8rem;
         right: 0.8rem;
         z-index: 99;
-        width: 70px;
-        height: auto;
+        border-radius: 100%;
+        width: 50px;
+        height: 50px;
         text-align: center;
         visibility: hidden;
 
-        @apply bg-arth-green hover:bg-arth-light-blue border-arth-green focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center;
+        @apply bg-arth-green hover:bg-arth-light-blue border-arth-green focus:outline-none focus:ring-4 font-bold
+        rounded-full text-sm  text-center text-black px-3 py-2;
     }
 
-    .scrolled-page {
+    .scrolled__page {
         visibility: initial;
     }
 
-    img {
-        transform: rotate(180deg);
-        width: 40px;
-        height: auto;
+    svg {
+        text-align: center;
     }
 
 </style>
