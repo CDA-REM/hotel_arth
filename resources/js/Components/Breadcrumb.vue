3670a48716c39c27de9551326c65c1b7b4cd8f6f
Emilie Ickart <emi.ickart@gmail.com>
2023-10-28 13:28:14 +0200

    Refactor - Add Breadcrumb component

---
+++ resources/js/Components/Breadcrumb.vue
@@ -0,0 +1,58 @@
+<template>
+    <nav class="flex" aria-label="Breadcrumb" id="form-breadcrumb">
+        <ul class="inline-flex items-center space-x-1 md:space-x-3">
+            <li class="inline-flex items-center ml-6">
+                <button @click="setActiveTab('checkAvailability')"
+                        :class="{ active: activeTab === 'checkAvailability' }"
+                        id="nav--button"
+                >
+                    {{ $t(('breadcrumb.reservation')) }}
+                </button>
+            </li>
+            <li> ></li>
+            <li class="inline-flex items-center ml-6">
+                <button @click="setActiveTab('selectOption')"
+                        :class="{ active: activeTab === 'selectOption' } "
+                        id="nav--button"
+                >
+                    {{ $t(('breadcrumb.options')) }}
+                </button>
+            </li>
+            <li> ></li>
+            <li class="inline-flex items-center ml-6">
+                <button @click="setActiveTab('validateBooking')"
+                        :class="{ active: activeTab === 'validateBooking' } "
+                        id="nav--button"
+                >
+                    {{ $t(('breadcrumb.validation')) }}
+                </button>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    props: {
+        activeTab: {
+            type: String,
+            required: true,
+        },
+    },
+    methods: {
+        //TODO - Terminate the breadcrumb code refactor
+        setActiveTab(tab) {
+            this.$emit('update:activeTab', tab);
+        },
+        nextTab() {
+            if (
+                this.activeTab !== "validateBooking" &&
+                this.$refs.reservationForm.reportValidity()
+            ) {
+                this.activeTab = this.allTabs[this.allTabs.indexOf(this.activeTab) + 1];
+            }
+        },
+    },
+};
+</script>
+
