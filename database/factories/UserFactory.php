d804c0bca7e125ea455d6b29ce9c1bd80d551d37
Emilie Ickart <emi.ickart@gmail.com>
2023-11-02 16:03:20 +0100

    Refactor - Replace role customer by user in user factory to match Voyager role

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
             'personal_address' => json_encode(["address" => "260 rue de l'Artichaut", "zip_code" => "74000", "city" => "Annecy"]),
             'enterprise_name' =>  'La Tortue',
             'professional_address' =>  json_encode(["address" => "18 Chemin du Moulin", "zip_code" => "74000", "city" => "Annecy"]),
-            'role' => 'customer',
+            'role' => 'user',
 //            'remember_token' => Str::random(10),
         ];
     }
