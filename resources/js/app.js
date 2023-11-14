6bb5c1bc578e14150ec95ef5489f9222e63c1e48
Emilie Ickart <emi.ickart@gmail.com>
2023-11-14 08:49:40 +0100

    Refactor - Required axios to use credentials for each request

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,18 +6,22 @@ import {createPinia} from "pinia";
 import router from "./router";
 import { createI18n } from "vue-i18n";
 import {translations} from "../translations/translations";
+import axios from 'axios';
 
 export const i18n = createI18n({
     locale: localStorage.lang || 'fr',
     messages: translations
 })
 
-createApp(App).use(router)
+createApp(App)
+    .use(router)
     .use(createPinia())
     .use(i18n)
     .mount('#app')
 
-
+// Configuration Axios
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = process.env.BASE_URL;
 // Axios interceptor modifies HTTP header before the request is sent back to Laravel
 axios.interceptors.request.use(
     function (config) {
